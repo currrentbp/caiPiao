c3c0537b42c05a9eb42bfdebc8f873544f7f4b3b
current_bp <1403271248@qq.com>
2017-10-08 21:30:50 +0800

    sss

--- src/main/java/com/bp/shuangseqiu/service/StrategyServiceImpl.java
+++ src/main/java/com/bp/shuangseqiu/service/StrategyServiceImpl.java
@@ -1,7 +1,13 @@
 package com.bp.shuangseqiu.service;
 
+import com.alibaba.fastjson.JSON;
 import com.bp.shuangseqiu.ShuangseqiuEntity;
+import com.bp.util.all.CheckUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
+import java.util.Map;
 
 /**
  * 策略
@@ -10,7 +16,7 @@ import java.util.List;
  * @createTime 20171006
  */
 public class StrategyServiceImpl {
-
+    private final static Logger logger = LoggerFactory.getLogger(StrategyServiceImpl.class);
 
     /**
      * 平均数方法获取双色球
@@ -18,7 +24,17 @@ public class StrategyServiceImpl {
      * @param num 获取双色球的数量
      * @return
      */
-    public List<ShuangseqiuEntity> averageStrategy(int num) {
+    public List<ShuangseqiuEntity> averageStrategy(int num,Integer id, List<ShuangseqiuEntity> localShuangseqiuEntities) {
+        if(CheckUtil.isEmpty(localShuangseqiuEntities)){
+            localShuangseqiuEntities = new LoadShuangseqiu().loadAllShuangseqiuFromLocal(null);
+        }
+
+        Map<Integer, Map<String, Map<Integer, Integer>>> repeatNums = new ShuangseqiuServiceImpl().getRepeatNums(num, localShuangseqiuEntities);
+
+        Map<Integer, Map<String, Integer>> totalRepeatProblem = new ShuangseqiuServiceImpl()
+                .getTotalRepeatProblem(num, localShuangseqiuEntities, repeatNums);
+
+
 
         return null;
     }
