964ba33d0bf814592a20b8ad64df934f5bb8e950
baopan <baopan@gaosiedu.com>
2018-04-13 09:59:52 +0800

    ss

--- src/main/java/com/currentbp/daletou/facade/ForecastDaletouServiceFacadeImpl.java
+++ src/main/java/com/currentbp/daletou/facade/ForecastDaletouServiceFacadeImpl.java
@@ -2,6 +2,8 @@ package com.currentbp.daletou.facade;
 
 import com.currentbp.api.daletou.facade.ForecastDaletouServiceFacade;
 import com.currentbp.entity.DaletouEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
@@ -12,10 +14,11 @@ import java.util.List;
  * @createTime 20180411
  */
 public class ForecastDaletouServiceFacadeImpl implements ForecastDaletouServiceFacade {
-
+    private final static Logger logger = LoggerFactory.getLogger(ForecastDaletouServiceFacadeImpl.class);
 
     @Override
     public List<DaletouEntity> forecastDaletou(int count, int daletouId) {
+        logger.info("===>forecastDaletou :" + daletouId);
         return null;
     }
 }
