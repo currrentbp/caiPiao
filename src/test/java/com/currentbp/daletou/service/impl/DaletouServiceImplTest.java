dacbb3a7f7ed70b1d35ec375f3c41c2c353ba247
baopan <baopan@gaosiedu.com>
2018-04-18 19:16:21 +0800

    sss

---
+++ src/test/java/com/currentbp/daletou/service/impl/DaletouServiceImplTest.java
@@ -0,0 +1,58 @@
+package com.currentbp.daletou.service.impl;
+
+import com.currentbp.BaseTest;
+import com.currentbp.daletou.bo.entity.DaletouBo;
+import com.currentbp.daletou.entity.Daletou;
+import com.currentbp.daletou.service.DaletouService;
+import com.currentbp.daletou.service.DownLoadDaletouHistoryService;
+import com.currentbp.util.all.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author current_bp
+ * @createTime 20180418
+ */
+public class DaletouServiceImplTest extends BaseTest {
+    private final static Logger logger = LoggerFactory.getLogger(DaletouServiceImplTest.class);
+
+    @Autowired
+    private DownLoadDaletouHistoryService daletouHistoryService;
+    @Autowired
+    private DaletouService daletouService;
+
+    @Test
+    public void insertAll() throws Exception {
+        List<DaletouBo> daletouBos = daletouHistoryService.downLoadAllDaletouHistory();
+        Assert.notNull(daletouBos, "===>is null");
+        for (DaletouBo daletouBo : daletouBos) {
+            Daletou daletou = null;
+            try {
+                daletou = daletouBo.toDaletou();
+            } catch (Exception e) {
+                continue;
+            }
+            daletouService.insert(daletou);
+        }
+    }
+
+    @Test
+    public void insertOne() throws Exception {
+        DaletouBo daletouBo = daletouHistoryService.downLoadDaletouHistory(11111);
+        Assert.notNull(daletouBo, "===>is null");
+        Daletou daletou = daletouBo.toDaletou();
+        daletouService.insert(daletou);
+    }
+
+
+    @Test
+    public void queryDaletouAll() throws Exception {
+    }
+
+}
