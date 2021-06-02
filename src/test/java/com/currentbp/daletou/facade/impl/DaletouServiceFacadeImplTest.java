4ecf43b531a258f9a3e05bf9f49dd6057f1940b4
baopan <baopan@xiaomi.com>
2021-06-02 17:13:02 +0800

    sss

--- src/test/java/com/currentbp/daletou/facade/impl/DaletouServiceFacadeImplTest.java
+++ src/test/java/com/currentbp/daletou/facade/impl/DaletouServiceFacadeImplTest.java
@@ -21,9 +21,9 @@ public class DaletouServiceFacadeImplTest extends BaseTest {
     @Test
     public void testForecastV2() {
         List<Daletou> daletous = new ArrayList<>();
-        daletous.add(new Daletou(1,6,12,24,34,7,9));
-        daletous.add(new Daletou(6,11,14,33,35,2,4));
-        List<Daletou> daletous1 = daletouServiceFacade.forecastV2(5, daletous, 21057);
+        daletous.add(new Daletou(7,23,33,34,35,7,9));
+        daletous.add(new Daletou(4,11,29,32,33,3,6));
+        List<Daletou> daletous1 = daletouServiceFacade.forecastV2(5, daletous, 21060);
         logger.info("===>result :"+ daletous1.toString());
     }
 }
